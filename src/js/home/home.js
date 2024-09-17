cbac9ce2fe1959110abe1d0b39cd7bfff8262a0f
Vitalie Rosescu <vitalier@hotmail.be>
2024-09-17 17:40:28 +0200

    button effect

--- src/js/home/home.js
+++ src/js/home/home.js
@@ -1,16 +1,8 @@
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-import addSplitText from './js/addSplitText'
-import heroScroll from './js/home/heroScroll'
-import homeMarquee from './js/home/homeMarquee'
-import homeProjects from './js/home/homeProjects'
-import loadingAnimation from './js/loadingAnimation'
-
 gsap.registerPlugin(ScrollTrigger)
 
-function home() {
-
-}
+function home() {}
 
 export default home
