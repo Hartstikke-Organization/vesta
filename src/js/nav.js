b522fc03200d3d6b593ed5f78b48fb3165268e6e
Vitalie Rosescu <vitalier@hotmail.be>
2024-09-27 16:01:09 +0200

    friyay

--- src/js/nav.js
+++ src/js/nav.js
@@ -9,13 +9,28 @@ function nav() {
     dropdown.addEventListener('mouseover', () => {
       if (dropdown.classList.contains('w--open')) {
         nav.querySelector('.nav_container').classList.add('with-h-padding')
+        nav.classList.add('is--active')
       }
     })
 
     dropdown.addEventListener('mouseout', () => {
-      if (!dropdown.classList.contains('w--open')) {
+      if (dropdown.classList.contains('w--open')) {
         nav.querySelector('.nav_container').classList.remove('with-h-padding')
+        nav.classList.remove('is--active')
       }
+    })
+  })
+
+  const dropdownLists = document.querySelectorAll('.nav_dropdown-list')
+  dropdownLists.forEach((dropdown) => {
+    dropdown.addEventListener('mouseover', () => {
+      nav.querySelector('.nav_container').classList.add('with-h-padding')
+      nav.classList.add('is--active')
+    })
+
+    dropdown.addEventListener('mouseout', () => {
+      nav.querySelector('.nav_container').classList.remove('with-h-padding')
+      nav.classList.remove('is--active')
     })
   })
 }
